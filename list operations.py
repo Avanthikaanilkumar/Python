b264c5933ded138fb3b2117b11ae9aa7d193df91
Avanthikaanilkumar <avanthikaanilkumar18@gmail.com>
2024-08-28 15:07:20 +0530

    Add files via upload

---
+++ list operations.py
@@ -0,0 +1,17 @@
+# list1=[12,14,16,18,20]
+# l=list1*2
+# print(l)
+# list1=[12,14,16]
+# list2=[9,6,7]
+# a=list1+list2
+# print('concatenation:',a)
+list1=[1,4,5,7,8]
+a=len(list1)
+print('length',a)
+list1=[12,14,16,39,40]
+for i in list1:
+    print(i)
+list1=[100,200,300,400]
+print(600 in list1)
+print(700 in list1)
+
