8823c2bdc88e9634e6a2a3979214b5abfc965211
Avanthikaanilkumar <avanthikaanilkumar18@gmail.com>
2024-08-28 15:16:56 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,24 @@
+Tupledata=(0,1,2,3,2,3,1,3,2)#creating tuple
+print(Tupledata)
+mytuple=()#Empty tuple
+print(mytuple)
+my_tuple=("mouse",[8,4,6],(1,2,3))
+print(my_tuple)#nested tuple
+letters=("p","r","o","g","a","m")#index
+print(letters[0])
+print(letters[5])
+letters=("p","r","o","g","a","m")#negative index
+print(letters[-1])
+print(letters[5])
+my_tuple=('p','r','o','g','r','a','m','i','z')#slicing
+print(my_tuple[1:4])
+print(my_tuple[:-7])
+print(my_tuple[7:])
+print(my_tuple[:])
+tuple=('python',"tuples")#repettion
+print("original tuple is:",tuple)
+tuple=tuple*3
+print("New tuple is:",tuple)
+mytuple=('a','p','p','i','e')#tuple methods
+print(mytuple.count('p'))
+print(mytuple.index("i"))
