fa4d75f3195caa807ded34acd1d86ad998bd00cb
Avanthikaanilkumar <avanthikaanilkumar18@gmail.com>
2024-09-05 18:02:55 +0530

    Add files via upload

---
+++ number is +,- or  0.py
@@ -0,0 +1,9 @@
+n=int(input("Enter the number:"))
+if(n>0):
+    print("The number is positive")
+else:
+    if(n<0):
+        print("The number is negative")
+    else:
+        if(n==0):
+            print("The number  is zero")
