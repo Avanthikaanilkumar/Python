ce6d33f4391858eefb8aa27256b1f14715a908c9
Avanthikaanilkumar <avanthikaanilkumar18@gmail.com>
2024-09-05 18:07:23 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+print(input("1:Addition 2.Subtraction 3.Multiplication 4.Division"))
+a=int("Enter your choice:")
+b=int(input("Enter the  first number:"))
+c=int(input("Enter the second number:"))
+if(a==1):
+    print("Answer:",b+c)
+elif(a==2):
+    print("Answer",b-c)
+elif(a==3):
+    print("Answer:",b*c)
+elif(a==4):
+    print("Answer:",b/c)
+
+
+
+
+
+
