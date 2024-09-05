8ccf8801e146999cd5854fc9bdfe8f42261244b6
Avanthikaanilkumar <avanthikaanilkumar18@gmail.com>
2024-09-05 18:04:29 +0530

    Add files via upload

---
+++ Largest of 3 numbers.py
@@ -0,0 +1,14 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enterr the second number:"))
+c=int(input("Enter the third number:"))
+if (a>b) :
+   if (a>c):
+        print("The largest number is",a)
+   else:
+        print("The largest number is:",c)
+else:
+    
+    if(b>c):
+        print("The largest number",b)
+    else:
+        print("The largest number",c)
