2cec3dcab1bf3c07380dfa9215469da4b4f9166a
Avanthikaanilkumar <avanthikaanilkumar18@gmail.com>
2024-09-05 18:52:49 +0530

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,11 @@
+a=float(input("Enter the weight"))
+b=float(input("Enter the height"))
+BMI=a/(b**2)
+if BMI<18.5:
+    print("Underweight")
+elif(18.5<=BMI<24.9):
+    print("Normal weight")
+elif(25<=BMI<29.9):
+    print("Overweight")
+else:
+    print("Obese")
