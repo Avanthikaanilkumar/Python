447d40c7ca146503843506014637c70f70e1d7a1
Avanthikaanilkumar <avanthikaanilkumar18@gmail.com>
2024-09-12 07:00:08 +0530

    Add files via upload

---
+++ Loop control stat.py
@@ -0,0 +1,13 @@
+# for string in "python Loops":
+#     if string=="o" or string=="p" or string=="t":
+#         continue
+#     print("Current Letter:",string)#continue
+
+# for string in "Python Loops":
+#     if string=='L':
+#         break
+#     print('Current Letter:',string)#break
+
+# for string in "python Loops":
+#     pass
+# print("Last Letter:",string)
